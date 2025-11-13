28a67c5451b29653096e37b9a37e08c07a1334b8
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-11-13 20:41:33 -0300

    Fazendo a função de trocar de tema

--- portifolio/src/app/page.js
+++ portifolio/src/app/page.js
@@ -1,18 +1,41 @@
 'use client';
-import { FaGithub } from "react-icons/fa";
-import { useState } from "react";
-import { BsSun, BsMoon } from "react-icons/bs";
+import {useState} from "react";
+
+import Topo from "@/componentes/Topo";
+import Apresentacao from "@/componentes/Apresentacao";
+import Sobre from "@/componentes/Sobre";
+import Projetos from "@/componentes/Projetos";
+import Contato from "@/componentes/Contato";
+import Rodape from "@/componentes/Rodape";
 import estilos from "./page.module.css";
 
 export default function Home() {
-  const [alterarTema, setAlterarTema] = useState(false);
+  const [ehTemaEscuro, setTemaEscuro] = useState(false);
 
-  function trocarTemaPagina() {
-    setAlterarTema()
+  function alterarTema() {
+    setTemaEscuro(!ehTemaEscuro)
   }
 
   return (
     <>
+      <header>
+        <Topo acao_onclick={alterarTema} ehTemaEscuro={ehTemaEscuro} />
+      </header>
+
+      <main>
+        <Apresentacao ehTemaEscuro={ehTemaEscuro} />
+        <Sobre ehTemaEscuro={ehTemaEscuro} />
+        <Projetos ehTemaEscuro={ehTemaEscuro} />
+      </main>
+
+      <section>
+        <Contato ehTemaEscuro={ehTemaEscuro} />
+      </section>
+
+      <footer>
+        <Rodape ehTemaEscuro={ehTemaEscuro} />
+      </footer>
+    
     </>
   );
 }
