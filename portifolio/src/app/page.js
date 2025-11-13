21dd5f6cf3acceb3f340b5774f95082bd9c8d52c
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-11-13 20:32:58 -0300

    Criando a estrutura de páginas

--- portifolio/src/app/page.js
+++ portifolio/src/app/page.js
@@ -1,6 +1,16 @@
+'use client';
+import { FaGithub } from "react-icons/fa";
+import { useState } from "react";
+import { BsSun, BsMoon } from "react-icons/bs";
 import estilos from "./page.module.css";
 
 export default function Home() {
+  const [alterarTema, setAlterarTema] = useState(false);
+
+  function trocarTemaPagina() {
+    setAlterarTema()
+  }
+
   return (
     <>
     </>
